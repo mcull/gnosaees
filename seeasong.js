a755d107de929b131c8d2380b49c468f57e06b8c
mcull <marc.cull@gmail.com>
2024-02-29 00:15:30 -0800

    Working script to fetch all the top songs for the week

--- seeasong.js
+++ seeasong.js
@@ -1,5 +1,140 @@
-const { getChart } = require('billboard-top-100');
-const { getLyrics } = require('genius-lyrics-api');
-require('dotenv').config();
+import { OpenAI } from 'openai';
+import { image as downloadImage } from 'image-downloader';
+import { getChart } from 'billboard-top-100';
+import { getLyrics } from 'genius-lyrics-api';
+import filenamify from 'filenamify';
+import { readFileSync, writeFileSync } from 'fs';
+import dayjs from 'dayjs' // ES 2015
+const today = dayjs().format()
 
-console.log("GENIUS:", process.env.GENIUS_API_KEY);
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const jsonPath = __dirname + '/songs.json';
+const songsJSON = readFileSync(jsonPath);
+let db = JSON.parse(songsJSON);
+console.log(db);
+
+import "dotenv/config.js";
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], 
+});
+  
+
+let imageOptions = {
+    url: 'http://someurl.com/image.jpg',
+    dest: '/path/to/dest',               // will be saved to /path/to/dest/image.jpg
+}; 
+
+const promptCarousel = 
+["award-winning photograph, Sigma 85 mm f/8, Slow Shutter Speed, Golden Hour Lighting, Uneven skin tone",
+"Canon EOS 1000D, ƒ/3.5, focal length: 18.0 mm, exposure time: 1/5, ISO 400, flash on, photograph of a photograph",
+"grainy expired film photo photograph from a magazine shoot ",
+"a slightly blurry black and white photograph medium distance high contrast of a puppet show",
+"child's drawing ",
+"stencil ",
+"Art Nouveau poster art ",
+"old timey newspaper ",
+"comic book ",
+"vintage advertisement ",
+"vintage postcard ",
+"glitchy VHS tape playing on an old 1980s CRT television with a VHS player ",
+"a tattoo",
+"Autochrome Lumière photograph ",
+"Surveillance & CCTV footage ",
+"Daguerrotype",
+"Disposable camera photograph ",
+"Vintage Polaroid photograph ",
+"Vintage 35mm photograph ",
+"Double exposure",
+"Drone photography",
+"Fashion photography",  
+"Film noir",
+"Found photography",
+"High dynamic range imaging",
+"High-speed photography",
+"Instant film",
+"Kinetic photography",
+"Light painting",
+"Low-key photography",
+"Macro photography",
+"Flash, harsh flash",
+"Lomography",
+"Long-exposure photography",
+"Pinhole photography, camera obscura"
+]
+
+const DELAY = 1000*15;
+const applyStyle = true;
+let drawings = 0;
+
+getChart('hot-100', '2024-02-24', (err, chart) => {
+    if (drawings > 0) { return; }
+    if (err) console.log(err);
+    let i = 5;
+    chart.songs.forEach((song, i) => {
+        setTimeout(() => {
+            const song = chart.songs[i];
+            const title = song.title;
+            const artist = song.artist;
+            console.log(`Title: ${title} Artist: ${artist}`);
+            if (!db.songs.find(song => song.title === title && song.artist === artist)) {
+                try {
+                    let songOptions = {
+                        apiKey: process.env.GENIUS_API_KEY,
+                        title: title,
+                        artist: artist,
+                        optimizeQuery: true
+                    };
+                    getLyrics(songOptions).then(async (lyrics) => 
+                    {
+                        console.log("got lyrics!");
+                        let image = null;
+                        let filename = null;
+                        let url = null;
+                        let error = null;
+                        let revisedPrompt = null
+                        
+                        try {
+                            let prompt = ` about ${title} by ${artist} that captures the tone and essential message of the following lyrics without using any typographical elements  ${lyrics}`;
+                            if (applyStyle) {
+                                let style = `${promptCarousel[Math.floor(Math.random() * promptCarousel.length)]}`;
+                                console.log(style);
+                                prompt = `${style} of ${prompt}`;
+                            }
+                           
+                            image = await openai.images.generate({ 
+                                model: "dall-e-3",
+                                prompt: prompt,
+                                n: 1,
+                                size: "1024x1024"
+                            });
+                            revisedPrompt = image.data[0].revised_prompt
+                            url = image.data[0].url;
+                            console.log(`got image ${url}`);
+                            filename = `${__dirname}/images/${filenamify(title + '_' + artist + '_' + today + '.png')}`;
+                            downloadImage({'url':url, 'dest': filename}).then(({ filename }) => {
+                                console.log('Saved to', filename); 
+                            }).catch((err) => console.error(err));
+                        } catch (openAIError) { 
+                            error = openAIError;
+                            console.log(error);
+                            console.log('Moving on to the next song');
+                        }
+                        db.songs.push({renderDate: today, title: title, artist: artist, lyrics: lyrics, image: filename, url: url, rewrittenPrompt: revisedPrompt, error: error});
+                        writeFileSync(jsonPath, JSON.stringify(db, null, 2), 'utf8');
+                        drawings++;
+                    });
+                } catch (lyricsError) {
+                    console.log(lyricsError);
+                }
+            }
+        }, i * DELAY);
+    });
+});
+
+
